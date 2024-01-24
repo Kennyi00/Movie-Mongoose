e778cbbd2ec556907a3a65c01393406fe56e0259
Isaiah Branstetter <isaiah.bran@icloud.com>
2024-01-23 19:41:44 -0500

    Added Movie Router Code

--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -3,6 +3,11 @@ const router = express.Router()
 const movieCtrl = require('../controllers/moviesController')
 
 
+router.post('/', movieCtrl.create)
+router.get('/', movieCtrl.index)
+router.get('/:id', movieCtrl.show)
+router.post('/:movieId/performers/:performerId', movieCtrl.addPerformer)
+
 
 
 
